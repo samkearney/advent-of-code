6255faea6fbecc7dc0cf056cc727f3c46026c5cd
Sam Kearney <samuelmkearney@gmail.com>
2019-12-22 22:09:42 -0500

    Day 6 Part 2

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -18,6 +18,41 @@ fn get_indirect_orbits(orbitee: &str, objects: &HashMap<String, String>) -> u32 
     }
 }
 
+fn get_santa_path(objects: &HashMap<String, String>) -> HashMap<String, u32> {
+    let mut santa_path : HashMap<String, u32> = HashMap::new();
+    let mut num_traversals = 0;
+    let mut orbitee = "SAN";
+    loop {
+        match objects.get(orbitee) {
+            Some(val) => {
+                santa_path.insert(val.to_string(), num_traversals);
+                num_traversals += 1;
+                orbitee = val;
+            },
+            None => return santa_path
+        }
+    }
+}
+
+fn get_traversals(objects: &HashMap<String, String>, santa_path: &HashMap<String, u32>) -> u32 {
+    let mut num_traversals = 0;
+    let mut orbitee = "YOU";
+    loop {
+        match objects.get(orbitee) {
+            Some(next_orbitee) => {
+                match santa_path.get(next_orbitee) {
+                    Some(intersection) => return num_traversals + intersection,
+                    None => {
+                        num_traversals += 1;
+                        orbitee = next_orbitee;
+                    }
+                }
+            }
+            None => panic!("Didn't find a path between YOU and SAN!")
+        }
+    }
+}
+
 fn main() {
     // Open the path in read-only mode, returns `io::Result<File>`
     let file = match File::open("input.txt") {
@@ -54,4 +89,8 @@ fn main() {
     }
 
     println!("Direct: {} Indirect: {} Total: {}", direct_orbits, indirect_orbits, direct_orbits + indirect_orbits);
+
+    // Grab the path from Santa to COM
+    let santa_path = get_santa_path(&objects);
+    println!("Traversals: {}", get_traversals(&objects, &santa_path));
 }
